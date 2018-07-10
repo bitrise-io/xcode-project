f38d2a7371d704d44a6b9cfa1266143c2baf3af7
Krisztián  Gödrei <godreikrisztian@gmail.com>
2018-07-10 13:19:10 +0200

    workspace file references

--- xcworkspace/xcworkspace_test.go
+++ xcworkspace/xcworkspace_test.go
@@ -11,42 +11,16 @@ import (
 func TestOpen(t *testing.T) {
 	workspaceContentsPth := testhelper.CreateTmpFile(t, "contents.xcworkspacedata", workspaceContentsContent)
 	workspacePth := filepath.Dir(workspaceContentsPth)
-	parentDir := filepath.Dir(workspacePth)
 
 	workspace, err := Open(workspacePth)
 	require.NoError(t, err)
 
 	require.Equal(t, filepath.Base(workspacePth), workspace.Name)
 	require.Equal(t, workspacePth, workspace.Path)
-
-	{
-		require.Equal(t, 1, len(workspace.FileRefs))
-		require.Equal(t, "group:XcodeProj.xcodeproj", workspace.FileRefs[0].Location)
-
-		pth, err := workspace.FileRefs[0].AbsPath(parentDir)
-		require.NoError(t, err)
-		require.Equal(t, filepath.Join(parentDir, "XcodeProj.xcodeproj"), pth)
-	}
-
-	{
-		require.Equal(t, 1, len(workspace.Groups))
-
-		group := workspace.Groups[0]
-		require.Equal(t, "group:Group", group.Location)
-
-		groupPth, err := group.AbsPath(parentDir)
-		require.NoError(t, err)
-		require.Equal(t, filepath.Join(parentDir, "Group"), groupPth)
-
-		require.Equal(t, 2, len(group.FileRefs))
-		require.Equal(t, "group:../XcodeProj/AppDelegate.swift", group.FileRefs[0].Location)
-		require.Equal(t, "group:XcodeProj.xcodeproj", group.FileRefs[1].Location)
-
-		groupFileRef := group.FileRefs[0]
-		groupFileRefPth, err := groupFileRef.AbsPath(groupPth)
-		require.NoError(t, err)
-		require.Equal(t, filepath.Join(parentDir, "XcodeProj/AppDelegate.swift"), groupFileRefPth)
-	}
+	require.Equal(t, 1, len(workspace.FileRefs))
+	require.Equal(t, "group:XcodeProj.xcodeproj", workspace.FileRefs[0].Location)
+	require.Equal(t, 1, len(workspace.Groups))
+	require.Equal(t, "group:Group", workspace.Groups[0].Location)
 }
 
 func TestIsWorkspace(t *testing.T) {
