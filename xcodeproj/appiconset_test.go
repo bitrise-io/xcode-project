0a01ee11f77d50c301381743fe6efeb49b9ebb28
lpusok <7979773+lpusok@users.noreply.github.com>
2020-11-17 16:44:14 +0100

    ACT-902 Looking up icons in apps with multipe asset catalogs (#47)
    * Iterate correctly over appiconsets.
    * Adding test case for looking up icons in projects with multiple asset catalogs

--- xcodeproj/appiconset_test.go
+++ xcodeproj/appiconset_test.go
@@ -1,9 +1,10 @@
 package xcodeproj
 
 import (
+	"fmt"
 	"io/ioutil"
 	"os"
-	"path"
+	"path/filepath"
 	"reflect"
 	"testing"
 
@@ -54,65 +55,107 @@ func Test_assetCatalog(t *testing.T) {
 	}
 }
 
-func Test_appIconSetPaths(t *testing.T) {
-	projectDir, err := ioutil.TempDir("", "ios-dummy-project")
-	if err != nil {
-		t.Errorf("setup: failed to create temp dir")
-	}
-	defer func() {
-		if err := os.RemoveAll(projectDir); err != nil {
-			t.Logf("Failed to clean up after test, error: %s", err)
-		}
-	}()
+type internalProject struct {
+	objects serialized.Object
+	proj    Proj
+}
 
-	appIconSetPath := path.Join(projectDir, "ios-simple-objc", "Images.xcassets", "AppIcon.appiconset")
-	if err := os.MkdirAll(appIconSetPath, 0755); err != nil {
-		t.Errorf("setup: failed top create dir %s", appIconSetPath)
+func createDummyProject(rawProj string, rootObjectID string, projectPath string, iconSetPaths [][]string) (internalProject, error) {
+	for _, pathElements := range iconSetPaths {
+		path := filepath.Join(append([]string{projectPath}, pathElements...)...)
+		if err := os.MkdirAll(path, 0755); err != nil {
+			return internalProject{}, fmt.Errorf("failed top create dir %v", err)
+		}
 	}
 
 	var objects serialized.Object
-	_, err = plist.Unmarshal([]byte(rawProj), &objects)
+	_, err := plist.Unmarshal([]byte(rawProj), &objects)
 	if err != nil {
-		t.Fatalf("setup: failed to unmarshal project")
+		return internalProject{}, fmt.Errorf("failed to unmarshal project, %v", err)
 	}
-	proj, err := parseProj("BA3CBE6D19F7A93800CED4D5", objects)
+	// PBXProject object ID
+	proj, err := parseProj(rootObjectID, objects)
 	if err != nil {
-		t.Fatalf("setup: failed to parse project")
+		return internalProject{}, fmt.Errorf("failed to parse project, %v", err)
 	}
 
-	type args struct {
-		project     Proj
-		projectPath string
-		objects     serialized.Object
-	}
+	return internalProject{
+		objects: objects,
+		proj:    proj,
+	}, nil
+}
+
+func Test_appIconSetPaths(t *testing.T) {
 	tests := []struct {
-		name    string
-		args    args
-		want    TargetsToAppIconSets
-		wantErr bool
+		name         string
+		rawProj      string
+		rootObjectID string
+		projPath     string
+		iconSetPaths [][]string
+		want         []string
+		wantErr      bool
 	}{
 		{
-			name: "happy case",
-			args: args{
-				project:     proj,
-				projectPath: path.Join(projectDir, "ios-simple-objc.xcodeproj"),
-				objects:     objects,
+			name:         "single asset catlog",
+			rawProj:      rawProj,
+			rootObjectID: "BA3CBE6D19F7A93800CED4D5",
+			projPath:     "ios-simple-objc.xcodeproj",
+			iconSetPaths: [][]string{
+				{"ios-simple-objc", "Images.xcassets", "AppIcon.appiconset"},
 			},
-			want: TargetsToAppIconSets{
-				proj.Targets[0].ID: []string{appIconSetPath},
+			want: []string{"ios-simple-objc", "Images.xcassets", "AppIcon.appiconset"},
+		},
+		{
+			name:         "asset catalog missing",
+			rawProj:      rawProj,
+			rootObjectID: "BA3CBE6D19F7A93800CED4D5",
+			projPath:     "ios-simple-objc.xcodeproj",
+			iconSetPaths: [][]string{},
+			want:         []string{},
+			wantErr:      true,
+		},
+		{
+			name:         "2 asset catalogs",
+			rawProj:      rawCatalystProj,
+			rootObjectID: "13917C0A243F43D00087912B",
+			projPath:     "Catalyst Sample.xcodeproj",
+			iconSetPaths: [][]string{
+				{"Catalyst Sample", "Assets.xcassets", "AppIcon.appiconset"},
+				{"Catalyst Sample", "Preview Content", "Preview Assets.appiconset"},
 			},
-			wantErr: false,
+			want: []string{"Catalyst Sample", "Assets.xcassets", "AppIcon.appiconset"},
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got, err := appIconSetPaths(tt.args.project, tt.args.projectPath, tt.args.objects)
+			projectDir, err := ioutil.TempDir("", "ios-dummy-project")
+			if err != nil {
+				t.Errorf("setup: failed to create temp dir, %v", err)
+			}
+			defer func() {
+				if err := os.RemoveAll(projectDir); err != nil {
+					t.Logf("failed to clean up after test, error: %s", err)
+				}
+			}()
+			internalProject, err := createDummyProject(tt.rawProj, tt.rootObjectID, projectDir, tt.iconSetPaths)
+			if err != nil {
+				t.Errorf("setup: %v", err)
+			}
+			var want TargetsToAppIconSets
+			if len(tt.want) != 0 {
+				want = TargetsToAppIconSets{
+					internalProject.proj.Targets[0].ID: []string{filepath.Join(append([]string{projectDir}, tt.want...)...)},
+				}
+			}
+
+			got, err := appIconSetPaths(internalProject.proj, filepath.Join(projectDir, tt.projPath), internalProject.objects)
+
 			if (err != nil) != tt.wantErr {
 				t.Errorf("appIconSetPaths() error = %v, wantErr %v", err, tt.wantErr)
 				return
 			}
-			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("appIconSetPaths() = %v, want %v", got, tt.want)
+			if !reflect.DeepEqual(got, want) {
+				t.Errorf("appIconSetPaths() = %v, want %v", got, want)
 			}
 		})
 	}
